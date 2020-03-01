cf457e253a174416af0bb4c9741853dc26b7f0da
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-03-01 19:42:38 +0700

    update tensaction

--- src/app/pages/transaction_input/transaction-input/transaction-input.page.ts
+++ src/app/pages/transaction_input/transaction-input/transaction-input.page.ts
@@ -11,59 +11,21 @@ import { Observable } from 'rxjs';
 })
 export class TransactionInputPage implements OnInit {
 
-  idea: Person = {
-    name: '',
-    notes: ''
-  };
-
   private type_input: string;
-  private ideas: Observable<Person[]>;
  
-  constructor(private activatedRoute: ActivatedRoute, private MPersonService: MPersonService,
-    private toastController: ToastController, private router: Router,private modalController: ModalController,
-    navParams: NavParams) { 
-      this.type_input = navParams.get('type_input')
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private router: Router,
+    private toastController: ToastController, 
+    private modalController: ModalController,
+    private navParams: NavParams,
+    private MPersonService: MPersonService
+  ) { 
+      this.type_input = navParams.get('type_input');
     }
  
   ngOnInit() {
-    this.ideas = this.MPersonService.getIdeas();
-    console.log(this.ideas.subscribe(res => console.log(res)))
    }
- 
-  ionViewWillEnter() {
-    let id = this.activatedRoute.snapshot.paramMap.get('id');
-    if (id) {
-      this.MPersonService.getIdea(id).subscribe(idea => {
-        this.idea = idea;
-      });
-    }
-  }
- 
-  addIdea() {
-    this.MPersonService.addIdea(this.idea).then(() => {
-      this.router.navigateByUrl('/');
-      this.showToast('Idea added');
-    }, err => {
-      this.showToast('There was a problem adding your idea :(');
-    });
-  }
- 
-  deleteIdea() {
-    this.MPersonService.deleteIdea(this.idea.id).then(() => {
-      this.router.navigateByUrl('/');
-      this.showToast('Idea deleted');
-    }, err => {
-      this.showToast('There was a problem deleting your idea :(');
-    });
-  }
- 
-  updateIdea() {
-    this.MPersonService.updateIdea(this.idea).then(() => {
-      this.showToast('Idea updated');
-    }, err => {
-      this.showToast('There was a problem updating your idea :(');
-    });
-  }
  
   showToast(msg) {
     this.toastController.create({
--- src/app/services/m_person/m-person.service.ts
+++ src/app/services/m_person/m-person.service.ts
@@ -4,27 +4,24 @@ import 'rxjs/add/operator/map';
 import { map, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/firestore';
-export interface Person {
-  id?: string,
+export interface MPerson {
+  per_id?: string,
   per_username: string,
-  per_password: string
+  per_password: string,
+  per_active: string
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class MPersonService   {
-  // public per_id : string;
-  // public per_username : string;
-  // public per_password : string;
-  // public per_active : string;
   
-  private ideas: Observable<Person[]>;
-  private ideaCollection: AngularFirestoreCollection<Person>;
+  private obs_mperson: Observable<MPerson[]>;
+  private mpersonCollection: AngularFirestoreCollection<MPerson>;
  
   constructor(private afs: AngularFirestore) {
-    this.ideaCollection = this.afs.collection<Person>('M_person');
-    this.ideas = this.ideaCollection.snapshotChanges().pipe(
+    this.mpersonCollection = this.afs.collection<MPerson>('M_person');
+    this.obs_mperson = this.mpersonCollection.snapshotChanges().pipe(
       map(actions => {
         return actions.map(a => {
           const data = a.payload.doc.data();
@@ -35,30 +32,30 @@ export class MPersonService   {
     );
   }
  
-  getIdeas(): Observable<Person[]> {
-    return this.ideas;
+  get_obs_mperson(): Observable<MPerson[]> {
+    return this.obs_mperson;
   }
  
-  getIdea(id: string): Observable<Person> {
-    return this.ideaCollection.doc<Person>(id).valueChanges().pipe(
+  get_obs_mperson_by_id(id: string): Observable<MPerson> {
+    return this.mpersonCollection.doc<MPerson>(id).valueChanges().pipe(
       take(1),
-      map(idea => {
-        idea.id = id;
-        return idea
+      map(M_person => {
+        M_person.per_id = id;
+        return M_person
       })
     );
   }
  
-  addIdea(idea: Person): Promise<DocumentReference> {
-    return this.ideaCollection.add(idea);
+  insert_person(mperson: MPerson): Promise<DocumentReference> {
+    return this.mpersonCollection.add(mperson);
   }
  
-  updateIdea(idea: Person): Promise<void> {
-    return this.ideaCollection.doc(idea.id).update({ per_username: idea.per_username, per_password: idea.per_password });
+  update_person(mperson: MPerson): Promise<void> {
+    return this.mpersonCollection.doc(mperson.per_id).update({ per_username: mperson.per_username, per_password: mperson.per_password });
   }
  
-  deleteIdea(id: string): Promise<void> {
-    return this.ideaCollection.doc(id).delete();
+  delete_person(per_id: string): Promise<void> {
+    return this.mpersonCollection.doc(per_id).delete();
   }
 
 }
