15d02377fae38c1ba88489f804af395998aab2a0
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-03-02 21:35:18 +0700

    update all view

--- src/app/tab_report/tab-report.page.ts
+++ src/app/tab_report/tab-report.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab-report',
@@ -7,6 +8,51 @@ import { Component } from '@angular/core';
 })
 export class TabReportPage {
 
-  constructor() {}
+  constructor(
+    private modalController: ModalController,
+    private alertController: AlertController
+  ) {}
+
+// * @Function   : selcet_report_alert => แสดง Select สำหรับเลือกประเภทรายงาน
+// * @Author     : Jiramate
+// * @Create Date: 2563-03-02
+async selcet_report_alert(){
+  const alert = await this.alertController.create({
+    header: 'Select time range',
+    buttons: [
+      {
+        text: 'Day',
+        cssClass: 'secondary',
+        handler: () => {
+        }
+      },
+      {
+        text: 'Week',
+        cssClass: 'secondary',
+        handler: () => {
+        }
+      },
+      {
+        text: 'Month',
+        cssClass: 'secondary',
+        handler: () => {
+        }
+      },
+      {
+        text: 'Year',
+        cssClass: 'secondary',
+        handler: () => {
+        }
+      },
+      {
+        text: 'Cancel',
+        cssClass: 'secondary',
+        handler: () => {
+        }
+      }
+    ]
+  });
+  await alert.present();
+}
 
 }
