19a3d31de12ecaaf56ea7ea74df436d3ef0c4b53
K-Utumporn <60160104@go.buu.ac.th>
2020-03-02 17:49:38 +0700

    update modal category

--- src/app/pages/category/category.page.ts
+++ src/app/pages/category/category.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
+import { CategoryInputPage } from './../category_input/category-input.page';
 @Component({
   selector: 'app-category',
   templateUrl: './category.page.html',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController
+  ) { }
 
   ngOnInit() {
+  }
+
+  // * @Function   : modal_insert_show => แสดง modal CategoryInputPage
+  // * @Author     : Kessarin
+  // * @Create Date: 2563-03-01
+  async modal_insert_show() {
+    const modal = await this.modalController.create({
+      component: CategoryInputPage,
+      componentProps: {
+        'type_input': 'insert'
+      }
+    });
+    return await modal.present();
   }
 
 }
--- src/app/pages/category_input/category-input.page.ts
+++ src/app/pages/category_input/category-input.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavParams, ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-category-input',
   templateUrl: './category-input.page.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryInputPage implements OnInit {
 
-  constructor() { }
+  private type_input: string;
+  constructor(
+    private navParams: NavParams,
+    private modalController: ModalController
+  ) { 
+    this.type_input = navParams.get('type_input');
+  }
 
   ngOnInit() {
+  }
+
+  // * @Function   : close_modal => คำสั่งปิด modal
+  // * @Author     : Kessarin
+  // * @Create Date: 2563-03-02
+  async close_modal(){
+    this.modalController.dismiss({
+      'dismissed': true
+    });
   }
 
 }
