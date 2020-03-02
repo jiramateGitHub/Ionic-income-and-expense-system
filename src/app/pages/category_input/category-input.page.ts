ad134a5cf62d48b6b863cb99fc9567dff0382380
Thananya Banchuenwijit <60160086@go.buu.ac.th>
2020-03-02 19:43:04 +0700

    update category input

--- src/app/pages/category_input/category-input.page.ts
+++ src/app/pages/category_input/category-input.page.ts
@@ -1,22 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams, ModalController } from '@ionic/angular';
+import { ModalController, ToastController, NavParams, AlertController } from '@ionic/angular';
+import { TransactionCategoryPage } from './../transaction_category/transaction-category.page';
 @Component({
   selector: 'app-category-input',
   templateUrl: './category-input.page.html',
   styleUrls: ['./category-input.page.scss'],
 })
 export class CategoryInputPage implements OnInit {
-
+  public catt_name: string;
   private type_input: string;
+  private category_type: string;
   constructor(
     private navParams: NavParams,
-    private modalController: ModalController
+    private toastController: ToastController, 
+    private modalController: ModalController,
+    private alertController: AlertController,
   ) { 
     this.type_input = navParams.get('type_input');
   }
 
   ngOnInit() {
   }
+
+// * @Function   : modal_taransaction_category_show => แสดง Modal TransactionCategoryPage และ ตอนปิด Modal จะ Passing Data กลับมา
+// * @Author     : Thananya
+// * @Create Date: 2563-03-02
+async modal_taransaction_category_show() {
+  const modal = await this.modalController.create({
+    component: TransactionCategoryPage,
+    componentProps: {
+      'type_input': this.category_type
+    }
+  });
+  modal.onDidDismiss()
+  .then((data) => {
+    this.catt_name = data['data'].name; // Here's your selected user!
+  });
+  return await modal.present();
+}
 
   // * @Function   : close_modal => คำสั่งปิด modal
   // * @Author     : Kessarin
