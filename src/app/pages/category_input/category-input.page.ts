69ce8a2dc5fcdf1cc75fe412d77376a8109ebe24
Thananya Banchuenwijit <60160086@go.buu.ac.th>
2020-03-09 20:31:03 +0700

    เพิ่ม insert update delete sub_categories

--- src/app/pages/category_input/category-input.page.ts
+++ src/app/pages/category_input/category-input.page.ts
@@ -1,3 +1,4 @@
+import { ServicesService, MSubCategories } from './../../services/services.service';
 import { Component, OnInit } from '@angular/core';
 import { ModalController, ToastController, NavParams, AlertController } from '@ionic/angular';
 import { TransactionCategoryPage } from './../transaction_category/transaction-category.page';
@@ -7,14 +8,23 @@ import { TransactionCategoryPage } from './../transaction_category/transaction-c
   styleUrls: ['./category-input.page.scss'],
 })
 export class CategoryInputPage implements OnInit {
-  public catt_name: string;
+  
   private type_input: string;
-  private category_type: string;
+  private type_parent_categorise : string;
+  public obj_MSubCategories: MSubCategories= {
+    id : null,
+    username: null,
+    categorise_name: null,
+    categorise_type: null,
+    sub_categories_name: null,
+    sub_categorise_active: null
+  }
   constructor(
     private navParams: NavParams,
     private toastController: ToastController, 
     private modalController: ModalController,
     private alertController: AlertController,
+    private ServicesService: ServicesService
   ) { 
     this.type_input = navParams.get('type_input');
   }
@@ -29,12 +39,13 @@ async modal_taransaction_category_show() {
   const modal = await this.modalController.create({
     component: TransactionCategoryPage,
     componentProps: {
-      'type_input': this.category_type
+      'type_input': this.type_parent_categorise
     }
   });
   modal.onDidDismiss()
   .then((data) => {
-    this.catt_name = data['data'].name; // Here's your selected user!
+    this.obj_MSubCategories.categorise_name = data['data'].name; // Here's your selected user!
+    this.obj_MSubCategories.categorise_type = data['data'].type;
   });
   return await modal.present();
 }
@@ -46,6 +57,16 @@ async modal_taransaction_category_show() {
     this.modalController.dismiss({
       'dismissed': true
     });
+  }
+
+  // * @Function   : insert_categories => คำสั่งเพิ่ม sub_categories
+  // * @Author     : Thananya Banchuenwijit
+  // * @Create Date: 2563-03-09
+  insert_categories(){
+    this.obj_MSubCategories.username = this.ServicesService.SessionService.get_session_username()
+    this.obj_MSubCategories.sub_categorise_active = "Y"
+    this.ServicesService.MSubCategoriesService.
+    //console.log(this.obj_MSubCategories)
   }
 
 }
--- src/app/services/m_sub_categories/m-sub-categories.service.ts
+++ src/app/services/m_sub_categories/m-sub-categories.service.ts
@@ -1,9 +1,42 @@
+import { MSubCategories } from './../services.service';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MSubCategoriesService {
 
-  constructor() { }
+  private service: Observable<MSubCategories[]>;
+
+  private serviceCollection: AngularFirestoreCollection<MSubCategories>;
+ 
+  constructor(
+    private afs: AngularFirestore) {
+  }
+
+  // * @Function   : insert_sub_categories => เพิ่มข้อมูล sub_categories ลงใน Firebase Cloud
+// * @Author     : Thananya Banchuenwijit
+// * @Create Date: 2563-03-09
+insert_sub_categories(msubcategories: MSubCategories): Promise<DocumentReference> {
+  return this.serviceCollection.add(msubcategories);
+}
+
+// * @Function   : update_sub_categories => แก้ไขข้อมูล sub_categories ลงใน Firebase Cloud
+// * @Author     : Thananya Banchuenwijit
+// * @Create Date: 2563-03-09
+update_sub_categories(msubcategories: MSubCategories): Promise<void> {
+  return this.serviceCollection.doc(msubcategories.id).update({ categorise_name: msubcategories.categorise_name, categorise_type: msubcategories.categorise_type, sub_categories_name: msubcategories.sub_categories_name });
+}
+
+// * @Function   : delete_sub_categories => ลบข้อมูล sub_categories ใน Firebase Cloud
+// * @Author     : Thananya Banchuenwijit
+// * @Create Date: 2563-03-09
+delete_sub_categories(id: string): Promise<void> {
+  return this.serviceCollection.doc(id).delete();
+}
 }
