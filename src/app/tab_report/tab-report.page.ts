26091952160f2a165f14b4020d35b658e25903ff
Sathien Supabkul <60160353@go.buu.ac.th>
2020-03-09 21:57:41 +0700

    update tab-report

--- src/app/tab_report/tab-report.page.ts
+++ src/app/tab_report/tab-report.page.ts
@@ -26,7 +26,9 @@ export class TabReportPage {
     private modalController: ModalController,
     private alertController: AlertController,
     private ServicesService : ServicesService
-  ) {}
+  ) {
+    this.get_report_by_day()
+  }
 
 // * @Function   : selcet_report_alert => แสดง Select สำหรับเลือกประเภทรายงาน
 // * @Author     : Jiramate
@@ -74,8 +76,10 @@ async selcet_report_alert(){
 // * @Author     : Sathien Supabkul
 // * @Create Date: 2563-03-09
 get_report_by_day(){
-  this.ServicesService.MTransactionService.get_obs_mtransaction(this.obj_MPerson).subscribe(async res => {
-    
+  this.obj_MTransaction.transaction_date = postTimestamp.dateValue()
+  this.ServicesService.MTransactionService.get_obs_mtransaction(this.obj_MTransaction).subscribe(async res => {
+    console.log(res)
+
   });
 }
 
