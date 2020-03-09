4c14b116b00f2a69c711a7877d7939a47a24cd42
neshelloworld <60160089@go.buu.ac.th>
2020-03-09 22:02:20 +0700

    add insert

--- src/app/pages/wallet_input/wallet-input.page.ts
+++ src/app/pages/wallet_input/wallet-input.page.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, ToastController, NavParams, AlertController } from '@ionic/angular';
 import { TransactionCategoryPage } from './../transaction_category/transaction-category.page';
+import { ServicesService, MWallet , MTransaction} from '../../services/services.service';
+
 @Component({
   selector: 'app-wallet-input',
   templateUrl: './wallet-input.page.html',
   styleUrls: ['./wallet-input.page.scss'],
 })
 export class WalletInputPage implements OnInit {
-  public catt_name: string;
+  public categories_name: string;
   private type_input: string;
+  public username: string;
+  public wallet_name: string;
+  public wallet_balance : string;
+  public wallet_active: string;
+  
+  public obj_MWallet: MWallet = {
+    username: null,
+    wallet_name: null,
+    wallet_balance: null,
+    wallet_active: null
+ 
+  };
+
+  public obj_MTransaction: MTransaction = {
+    username : null,
+    wallet_name : null,
+    categorise_type : null,
+    categorise_name : null,
+    sub_categories_name : null,
+    transaction_amount : null,
+    transaction_date :null,
+    transaction_note : null,
+    transaction_active :null
+ 
+  };
+
   constructor(
     private navParams: NavParams,
     private modalController: ModalController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    
   ) { 
     this.type_input = navParams.get('type_input');
   }
@@ -64,7 +93,7 @@ export class WalletInputPage implements OnInit {
     });
     modal.onDidDismiss()
     .then((data) => {
-      this.catt_name = data['data'].name; // Here's your selected user!
+      this.categories_name = data['data'].name; // Here's your selected user!
     });
     return await modal.present();
   }
@@ -76,5 +105,17 @@ export class WalletInputPage implements OnInit {
     this.modalController.dismiss({
       'dismissed': true
     });
+  }
+  // * @Function   : insert_model => เพิ่มกระเป๋าเงิน
+  // * @Author     : Netchanok Thaintin
+  // * @Create Date: 2563-03-09
+  async insert_wallet(){
+    console.log(this.wallet_name , this.wallet_balance);
+    this.obj_MWallet.wallet_name = this.wallet_name;
+    this.obj_MWallet.wallet_balance = this.wallet_balance;
+    this.obj_MTransaction.transaction_amount = this.wallet_balance;
+    this.obj_MTransaction.transaction_note = "...";
+    this.obj_MTransaction.transaction_active = "Y";
+    // this.obj_MWallet.categories_name = this.categories_name;
   }
 }
