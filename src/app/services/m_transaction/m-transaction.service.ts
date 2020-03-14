5eeded083b1d5ed1dd3461dd7a5e0ae6bc18aa45
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-03-14 13:41:57 +0700

    update transaction

--- src/app/services/m_transaction/m-transaction.service.ts
+++ src/app/services/m_transaction/m-transaction.service.ts
@@ -43,7 +43,7 @@ export class MTransactionService {
   // * @Author     : Kanathip Phithaksilp
   // * @Create Date: 2563-03-10
   get_all_transaction_show():Observable<MTransaction[]>{
-    this.serviceCollection = this.afs.collection<MTransaction>('M_transaction', ref => ref.where('username','==',this.SessionService.get_session_username()).orderBy('transaction_date','desc'));
+    this.serviceCollection = this.afs.collection<MTransaction>('M_transaction', ref => ref.where('username','==',this.SessionService.get_session_username()));
     this.service = this.serviceCollection.snapshotChanges().pipe(
       map(actions => {
         return actions.map(a => {
