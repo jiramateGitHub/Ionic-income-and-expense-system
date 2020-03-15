baa310b9f9e6c89bf9166ccfceb5b24d70b76dcd
Kanathip Phithaksilp <60160154@go.buu.ac.th>
2020-03-15 16:00:12 +0700

    Sort

--- src/app/tab_wallet/tab-wallet.page.ts
+++ src/app/tab_wallet/tab-wallet.page.ts
@@ -55,6 +55,9 @@ export class TabWalletPage implements OnInit{
     this.get_all_transaction_show();
   }
 
+   // * @Function   : modal_edit_show => Modal edit
+  // * @Author     : Kanathip Phithaksilp
+  // * @Create Date: 2563-03-06
   async modal_edit_show(id:string) {
     await this.servicesService.MTransactionService.get_edit_transaction(id).subscribe( async res => {
       this.edit_transaction = res;
@@ -79,6 +82,9 @@ export class TabWalletPage implements OnInit{
     
   }
 
+  // * @Function   : transaction_active_update_AlertConfirm => แจ้งเตือนการลบ
+  // * @Author     : Kanathip Phithaksilp
+  // * @Create Date: 2563-03-06
   async transaction_active_update_AlertConfirm(id:string) {
     const alert = await this.alertController.create({
       header: 'Confirm Delete?',
@@ -106,12 +112,23 @@ export class TabWalletPage implements OnInit{
   // * @Create Date: 2563-03-06
   async get_all_transaction_show(){
       this.servicesService.MTransactionService.get_all_transaction_show().subscribe( res => {
+      console.log(res)
       this.all_transaction = res;
-      console.log( this.all_transaction)
-
+      // console.log( this.all_transaction)
    })
    
   }
+
+  // * @Function   : sortData => เรียงข้อมูลตามเวลา
+  // * @Author     : Kanathip Phithaksilp
+  // * @Create Date: 2563-03-06
+  get sortData() {
+    return this.all_transaction.sort((a, b) => {
+      return <any>new Date(b.transaction_date) - <any>new Date(a.transaction_date);
+    });
+  }
+
+
   // * @Function   : delete_transaction => ลบข้อมูล Transaction 
   // * @Author     : Thanpisit Suetrong
   // * @Create Date: 2563-03-11
